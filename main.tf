89d2f0eb26bafbebbd25c73d167374d5e458e442
lokesh udatha <138110408+lokeshudatha@users.noreply.github.com>
2025-12-10 15:08:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 provider "google" {
-  project     = "winter-monolith-477705-m8"
-  region      = "us-central1"
+  project = "winter-monolith-477705-m8"
+  region  = "us-central1"
 }
 
+# -----------------------------
+# VPC Network
+# -----------------------------
 resource "google_compute_network" "lokinetwork" {
   name                    = "lokesh-network"
-  auto_create_subnetworks = false 
+  auto_create_subnetworks = false
 }
 
+# -----------------------------
+# Subnetwork
+# -----------------------------
 resource "google_compute_subnetwork" "lokisubnetwork" {
   name          = "lokesh-subnetwork"
   network       = google_compute_network.lokinetwork.id
@@ -15,6 +21,9 @@ resource "google_compute_subnetwork" "lokisubnetwork" {
   ip_cidr_range = "10.0.0.0/22"
 }
 
+# -----------------------------
+# VM Instance
+# -----------------------------
 resource "google_compute_instance" "loki" {
   name         = "lokesh-udatha"
   zone         = "us-central1-a"
@@ -27,28 +36,23 @@ resource "google_compute_instance" "loki" {
   }
 
   network_interface {
-    subnetwork   = google_compute_subnetwork.lokisubnetwork.id
-    access_config {}
+    subnetwork = google_compute_subnetwork.lokisubnetwork.id
+
+    access_config {}   # Enables public IP
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 
   depends_on = [
     google_compute_subnetwork.lokisubnetwork
   ]
-  metadata_startup_script = <<-EOF
-    #!/bin/bash
-    apt update -y
-    apt install -y docker.io git
-    systemctl start docker
-    systemctl enable docker
-
-    # Clone your repository
-    git clone https://github.com/lokeshudatha/four /opt/four
-
-    # Build Docker Image
-    cd /opt/four
-    docker build -t  four-image:latest .
-  EOF
 }
+
+# -----------------------------
+# Firewall Rule
+# -----------------------------
 resource "google_compute_firewall" "lokifirewall" {
   name    = "lokesh-firewall"
   network = google_compute_network.lokinetwork.id
@@ -56,10 +60,34 @@ resource "google_compute_firewall" "lokifirewall" {
   direction = "INGRESS"
 
   allow {
-    protocol = "all"
+    protocol = "tcp"
+    ports    = ["22", "80", "443", "8080"]
   }
 
   source_ranges = ["0.0.0.0/0"]
 }
 
+# -----------------------------
+# Outputs for Harness Build Stage
+# -----------------------------
 
+# Public IP of VM
+output "vm_public_ip" {
+  value = google_compute_instance.loki.network_interface[0].access_config[0].nat_ip
+}
+
+# Private IP (if needed)
+output "vm_private_ip" {
+  value = google_compute_instance.loki.network_interface[0].network_ip
+}
+
+# SSH User
+output "ssh_username" {
+  value = "ubuntu"
+}
+
+# SSH Private Key (needed for Harness Build Stage)
+output "ssh_private_key" {
+  value     = file("~/.ssh/id_rsa")
+  sensitive = true
+}
